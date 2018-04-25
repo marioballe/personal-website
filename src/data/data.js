bb85be15815f796fc6364e319c24bef1618a7db5
@marioballe <ballestercalleja@gmail.com>
2018-04-25 19:51:29 +0200

    I implement a new folder structure and i start to develop a custom Sass

---
+++ src/data/data.js
@@ -0,0 +1,3 @@
+let socialMedia = [{ title: 'Github', url: 'https://github.com/marioballe', icon: 'fa fa-github' },
+            { title: 'Twitter', url: 'https://twitter.com/marioballe89', icon: 'fa fa-twitter' },
+            { title: 'Spotify', url: 'https://open.spotify.com/user/hdmario?si=87eqMTk0RIaP_3t6lE7QUw', icon: 'fa fa-spotify' }]
